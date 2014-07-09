0c476c98a3e05ed6ba26df0c4600ac0f08e116bd
thanhcong <johndoe@example.com>
2014-07-09 13:29:39 +0700

    Finish user sigup

--- Gemfile
+++ Gemfile
@@ -6,11 +6,13 @@ gem 'rails', '4.1.4'
 # Use sqlite3 as the database for Active Record
 gem 'bootstrap-sass', '2.3.2.0'
 gem 'sprockets', '2.11.0'
-
+gem 'bcrypt-ruby', '3.1.2'
+gem 'minitest'
 
 group :development, :test do
   gem 'sqlite3', '1.3.8'
   gem 'rspec-rails', '2.13.1'
+  gem 'factory_girl_rails', '4.2.0'
 end
 
 group :test do
--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,15 @@
 Rails.application.routes.draw do
- get "users/new"
+
+  resources :users
+  get "users/new"
 
   root  'static_pages#home'
   match '/signup',  to: 'users#new',            via: 'get'
   match '/help',    to: 'static_pages#help',    via: 'get'
   match '/about',   to: 'static_pages#about',   via: 'get'
   match '/contact', to: 'static_pages#contact', via: 'get'
+
+  I18n.enforce_available_locales = true
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
